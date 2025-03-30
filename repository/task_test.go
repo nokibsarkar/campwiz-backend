55360c80f9770907c18ae2133d56860409a2e6be
nokibsarkar <nokibsarkar@gmail.com>
2025-03-31 02:24:59 +0600

    added task sucess

--- repository/task_test.go
+++ repository/task_test.go
@@ -5,6 +5,9 @@ import (
 	"nokib/campwiz/models"
 	"nokib/campwiz/repository"
 	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
 )
 
 func TestTaskFindByIDError(t *testing.T) {
@@ -28,7 +31,7 @@ func TestTaskFindByIDError(t *testing.T) {
 		t.Errorf("expected error, got nil")
 	}
 }
-func TestTaskCreateError(t *testing.T) {
+func TestTaskCreateSuccess(t *testing.T) {
 	// Initialize the database connection
 	db, mock, close := repository.GetTestDB()
 	defer close()
@@ -37,13 +40,27 @@ func TestTaskCreateError(t *testing.T) {
 
 	// Mock the database behavior
 	testTask := &models.Task{
-		TaskID: "testtask",
+		TaskID:               "testtask",
+		Type:                 models.TaskTypeDistributeEvaluations,
+		CreatedAt:            time.Now(),
+		UpdatedAt:            time.Now(),
+		AssociatedRoundID:    nil,
+		AssociatedCampaignID: nil,
+		AssociatedUserID:     nil,
+		Data:                 nil,
+		SuccessCount:         0,
 	}
-	// mock.ExpectBegin()
-	mock.ExpectQuery("INSERT INTO").
-		WithArgs(testTask.TaskID, 1).
-		WillReturnError(fmt.Errorf("record not found"))
-	// mock.ExpectRollback()
+	mock.ExpectBegin()
+	mock.ExpectExec("INSERT INTO `tasks`").
+		//(`task_id`,`type`,`status`,`associated_round_id`,`associated_campaign_id`,`associated_user_id`,`data`,`created_at`,`updated_at`,`success_count`,`failed_count`,`failed_ids`,`remaining_count`,`created_by_id`)
+		WithArgs(testTask.TaskID, testTask.Type, testTask.Status,
+			testTask.AssociatedRoundID, testTask.AssociatedCampaignID, testTask.AssociatedUserID,
+			testTask.Data, testTask.CreatedAt,
+			testTask.UpdatedAt,
+			testTask.SuccessCount, testTask.FailedCount,
+			nil, testTask.RemainingCount, testTask.CreatedByID).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
 	_, err := taskRepo.Create(db, testTask)
 	if err == nil {
 		t.Errorf("expected error, got nil")

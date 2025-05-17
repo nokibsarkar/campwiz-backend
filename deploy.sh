8fe8c0a1a5c58832952cba9a7b4964e6bfd6203a
nokibsarkar <nokibsarkar@gmail.com>
2025-05-18 04:59:56 +0600

    trying to do the shit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 become campwiz-backend
-toolforge build start -L https://gitlab.wikimedia.org/nokibsarkar/campwiz-backend.git --ref $1
+toolforge build start -L https://gitlab.wikimedia.org/nokibsarkar/campwiz-backend.git 
 toolforge webservice buildservice restart --mount all
 toolforge jobs load jobs.yaml
 toolforge jobs restart campwiz-task-manager

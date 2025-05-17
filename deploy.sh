769f3ff54a636d3f0dd2edf21c99f601b9857467
nokibsarkar <nokibsarkar@gmail.com>
2025-05-18 04:56:51 +0600

    Added jobs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 become campwiz-backend
 toolforge build start -L https://gitlab.wikimedia.org/nokibsarkar/campwiz-backend.git --ref $1
 toolforge webservice buildservice restart --mount all
+toolforge jobs load jobs.yaml
+toolforge jobs restart campwiz-task-manager
 exit
 exit

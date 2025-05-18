6ca1cc1641c2390b0ddbe1d0f5c677d90ec84ce6
nokibsarkar <nokibsarkar@gmail.com>
2025-05-19 00:42:35 +0600

    Added flag parsing

--- main_test.go
+++ main_test.go
@@ -1,10 +1,23 @@
-package main
+package main_test
 
-// func TestHome(t *testing.T) {
-// 	var router = SetupRouter(true)
-// 	w := httptest.NewRecorder()
-// 	req, _ := http.NewRequest("GET", "/", nil)
-// 	router.ServeHTTP(w, req)
-// 	assert.Equal(t, 200, w.Code)
-// 	assert.NotEqual(t, "", w.Body.String())
-// }
+import (
+	"flag"
+	"log"
+)
+
+//	func TestHome(t *testing.T) {
+//		var router = SetupRouter(true)
+//		w := httptest.NewRecorder()
+//		req, _ := http.NewRequest("GET", "/", nil)
+//		router.ServeHTTP(w, req)
+//		assert.Equal(t, 200, w.Code)
+//		assert.NotEqual(t, "", w.Body.String())
+//	}
+func init() {
+	testFile := flag.String("test.testlogfile", "", "The test file to run")
+	flag.Parse()
+	if *testFile != "" {
+		// Run the test file
+		log.Printf("Running test file: %s", *testFile)
+	}
+}

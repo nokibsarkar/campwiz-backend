c8b9519df7a267191c521f81d466cec90df16e75
nokibsarkar <nokibsarkar@gmail.com>
2025-05-20 01:12:41 +0600

    Added pipeline for proxy thing

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 become campwiz-backend
+toolforge build start -L --image-name campwiz-backend-fake-nginx https://gitlab.wikimedia.org/nokibsarkar/campwiz-backend.git --ref fake-nginx
 toolforge build start -L https://gitlab.wikimedia.org/nokibsarkar/campwiz-backend.git 
-toolforge webservice buildservice restart --mount all
+toolforge webservice buildservice restart --mount all --buildservice-image tool-campwiz-backend/campwiz-backend-fake-nginx:latest
 toolforge jobs load jobs.yaml
 toolforge jobs restart campwiz-task-manager
 toolforge jobs restart campwiz-backend-readonly

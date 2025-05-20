4992ff4c1d58230b9b1a5f54a990457fcab15152
nokibsarkar <nokibsarkar@gmail.com>
2025-05-21 05:28:51 +0600

    Added page pile

---
+++ services/round_service/task-manager/import-sources/import-pagepile.go
@@ -0,0 +1,8 @@
+// This integration would require the following:
+// pagepile id
+// target round id
+// Pagepile sqlite is located at /data/project/shared/pagepile/
+// But Unfortunately Pagepile do not save page id, it only
+// saves the title
+// So, we have to implement the file name to page id
+package importsources

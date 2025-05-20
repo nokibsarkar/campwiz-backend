f4a1bf10fe5d54542134c7664e25bbfb887dbe1f
nokibsarkar <nokibsarkar@gmail.com>
2025-05-21 03:32:13 +0600

    Go there

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
 become campwiz-backend
 toolforge build start -L --image-name campwiz-backend-fake-nginx https://gitlab.wikimedia.org/nokibsarkar/campwiz-backend.git --ref fake-nginx
+toolforge webservice buildservice restart --mount all --buildservice-image tool-campwiz-backend/campwiz-backend-fake-nginx:latest
+exit
+exit

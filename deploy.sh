e350c4f3653115c5c355d0ea05e3fc953c5cb7f4
nokibsarkar <nokibsarkar@gmail.com>
2025-05-21 03:31:41 +0600

    No need to restart webservice

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 become campwiz-backend
 toolforge build start -L https://gitlab.wikimedia.org/nokibsarkar/campwiz-backend.git 
-toolforge webservice buildservice restart --mount all --buildservice-image tool-campwiz-backend/campwiz-backend-fake-nginx:latest
 toolforge jobs load jobs.yaml
 toolforge jobs restart campwiz-task-manager
 toolforge jobs restart campwiz-backend-readonly

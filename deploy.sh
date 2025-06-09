4ed5c1cbcfca0489158eec36d7117b9ff730a77b
nokibsarkar <nokibsarkar@gmail.com>
2025-06-09 07:37:15 +0600

    Refactor deploy.sh: parameterize TOOLNAME and BRANCH for improved flexibility

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-become campwiz-backend
-toolforge build start -L https://gitlab.wikimedia.org/nokibsarkar/campwiz-backend.git 
+TOOLNAME="$1" or "campwiz-backend"
+BRANCH="$2" or "main"
+become "$TOOLNAME"
+toolforge build start -L https://gitlab.wikimedia.org/nokibsarkar/campwiz-backend.git -b "$BRANCH"
 toolforge jobs load jobs.yaml
 toolforge jobs restart campwiz-task-manager
 toolforge jobs restart campwiz-backend-readonly

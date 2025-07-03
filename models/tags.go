4326b7bcaef14e5d3d2bbba67cb74b480a047d34
nokibsarkar <nokibsarkar@gmail.com>
2025-07-03 23:42:18 +0600

    fix: update Tag model to correctly specify foreign key constraint for Campaign

--- models/tags.go
+++ models/tags.go
@@ -3,5 +3,5 @@ package models
 type Tag struct {
 	Name       string   `gorm:"primaryKey;type:varchar(255);not null" json:"name"`
 	CampaignId int      `gorm:"primaryKey;not null" json:"campaignId"`
-	Campaign   Campaign `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"-"`
+	Campaign   Campaign `gorm:"foreignKey:CampaignId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"-"`
 }
